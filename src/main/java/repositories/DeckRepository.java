4a36ff7b2188406d0b9637e82a52686f93278ef7
KauanMO <kauaanmatheus@gmail.com>
2025-05-23 17:39:20 -0300

    feat: find decks by user id

--- src/main/java/repositories/DeckRepository.java
+++ src/main/java/repositories/DeckRepository.java
@@ -4,11 +4,16 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import models.Deck;
 
+import java.util.List;
 import java.util.Optional;
 
 @ApplicationScoped
 public class DeckRepository implements PanacheRepository<Deck> {
     public Optional<Deck> findByIdAndUserId(Long id, Long userId) {
         return Optional.of(this.find("id = ?1 and user.id = ?2", id, userId).firstResult());
+    }
+
+    public List<Deck> findByUserId(Long userId) {
+        return this.list("user.id = ?1", userId);
     }
 }
