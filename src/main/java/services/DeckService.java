34931cf56a3dc95a6b7219a9fa1cb1f3b561d9e7
KauanMO <kauaanmatheus@gmail.com>
2025-07-28 18:40:19 -0300

    feat: returning deck extra info in get my decks

--- src/main/java/services/DeckService.java
+++ src/main/java/services/DeckService.java
@@ -1,17 +1,26 @@
 package services;
 
+import enums.CardSubtypeEnum;
+import enums.CardTypeEnum;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import models.Deck;
-import models.User;
+import models.*;
 import repositories.DeckRepository;
+import rest.dtos.card.OutCardDTO;
 import rest.dtos.deck.CreateDeckDTO;
+import rest.dtos.deck.DeckExtraInfoDTO;
+import rest.dtos.deck.OutDeckDTO;
+import services.exceptions.CardNotFoundException;
 import services.exceptions.DeckNotFoundException;
 import services.exceptions.UserNotFoundException;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class DeckService {
@@ -44,12 +53,40 @@ public class DeckService {
         return deckFound;
     }
 
-    public List<Deck> findByUserId(Long userId) {
+    public List<DeckExtraInfoDTO> findByUserId(Long userId) {
         Long finalUserId = userId == null
                 ? tokenService.getUserId()
                 : userId;
 
-        return repository.findByUserId(finalUserId);
+        return findExtraInfoByUserId(finalUserId);
+    }
+
+    public List<DeckExtraInfoDTO> findExtraInfoByUserId(Long userId) {
+        List<Deck> decks = repository.findByUserId(userId);
+
+        List<DeckExtraInfoDTO> decksInfo = new ArrayList<>();
+
+        for (Deck deck : decks) {
+            Map<CardTypeEnum, Long> typesCount = deck.getCards().stream()
+                    .flatMap(dc -> dc.getCard().getShopCard().getTypes().stream())
+                    .collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+
+            long maxTypeCount = typesCount.values().stream().max(Long::compare).orElse(0L);
+
+            List<String> mainTypes = typesCount.entrySet().stream()
+                    .filter(e -> e.getValue() == maxTypeCount)
+                    .map(m -> m.getKey().name())
+                    .toList();
+
+            Card mainCard = deck.getCards().stream()
+                    .max(Comparator.comparing(dc -> dc.getCard().getPrice()))
+                    .get()
+                    .getCard();
+
+            decksInfo.add(new DeckExtraInfoDTO(new OutDeckDTO(deck), mainTypes, new OutCardDTO(mainCard)));
+        }
+
+        return decksInfo;
     }
 
     public Deck findByIdUserId(Long id, Long userId) {
